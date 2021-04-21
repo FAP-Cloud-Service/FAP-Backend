1cc4365a6cb978fa3aa7e64878605decf44a5a33
maltemorgenstern <65773564+maltemorgenstern@users.noreply.github.com>
2021-04-21 22:04:26 +0200

    Add basic structure for login and logout function

---
+++ FriendsAndPlaces/Functions/LoginFunction.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace FriendsAndPlaces.Functions
+{
+    public static class LoginFunction
+    {
+        [FunctionName("Login")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            // Check headers -> HTTP 415
+
+            // Check parameters -> HTTP 400
+
+            // Get user from database
+
+            // If user does not exist -> HTTP 401
+
+            // Compare passwords
+
+            // Passwords do not match -> HTTP 401
+
+            // Generate session
+
+            // Save session in database
+
+            // Return session in response body
+
+            return new OkObjectResult(null);
+        }
+    }
+}
+
