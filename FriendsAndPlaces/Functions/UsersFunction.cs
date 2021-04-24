91af1f1b7b23429a25789eaefebf2e4f9a98b41e
maltemorgenstern <65773564+maltemorgenstern@users.noreply.github.com>
2021-04-24 19:35:23 +0200

    Implement users function

--- FriendsAndPlaces/Functions/UsersFunction.cs
+++ FriendsAndPlaces/Functions/UsersFunction.cs
@@ -1,16 +1,27 @@
+using FriendsAndPlaces.Helpers.Database;
+using FriendsAndPlaces.Models.Users;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
 
 namespace FriendsAndPlaces.Functions
 {
     public class UsersFunction
     {
-        public UsersFunction()
-        {
+        private readonly IDatabaseManager _databaseManager;
 
+        private const string _acceptHeaderApplicationJson = "application/json";
+        private const string _contentTypeHeaderApplicationJson = "application/json";
+
+        public UsersFunction(IDatabaseManager databaseManager)
+        {
+            _databaseManager = databaseManager;
         }
 
         [FunctionName("CheckUsername")]
@@ -18,13 +29,28 @@ namespace FriendsAndPlaces.Functions
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "users/available")] HttpRequest req,
             ILogger log)
         {
-            // Validate query parameter (id) -> HTTP 404
+            // Get query parameter
+            string loginName = req.Query["id"];
 
-            // try get user from database
+            // Validate parameters
+            if (string.IsNullOrWhiteSpace(loginName))
+            {
+                return new BadRequestResult();
+            }
 
-            // return OK: {erbegnis: [true|false]}
+            // Get user from database
+            var user = _databaseManager.GetUser(loginName);
 
-            return new OkObjectResult(null);
+            if (user == null)
+            {
+                // loginName is available
+                return new OkObjectResult(new AvailabilityResponse() { Result = true });
+            }
+            else
+            {
+                // loginName is already taken
+                return new OkObjectResult(new AvailabilityResponse() { Result = false });
+            }
         }
 
         [FunctionName("CreateUser")]
@@ -32,17 +58,36 @@ namespace FriendsAndPlaces.Functions
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "users/new")] HttpRequest req,
             ILogger log)
         {
-            // Validate Accept and Content-Type header -> HTTP 415
+            // Check if Content-Type: application/json is present 
+            bool contentTypeHeaderExists = req.Headers.TryGetValue("Content-Type", out StringValues contentTypeHeaders);
+            if (contentTypeHeaderExists && !contentTypeHeaders[0].Equals(_contentTypeHeaderApplicationJson))
+            {
+                return new UnsupportedMediaTypeResult();
+            }
 
-            // Read json body and convert to User object
+            // Read request body
+            string requestBody = new StreamReader(req.Body).ReadToEndAsync().Result;
+            var user = JsonConvert.DeserializeObject<User>(requestBody);
 
-            // Optional: Check if all attributes are present
+            // Check if key properties are missing
+            if (user == null ||
+                user.Password == null ||
+                string.IsNullOrEmpty(user.LoginName) ||
+                string.IsNullOrEmpty(user.Password.Password))
+            {
+                return new BadRequestResult();
+            }
 
             // Create user in database
+            var success = _databaseManager.CreateUser(user);
 
-            // return 200 - OK
+            // Check if database creation was successful
+            if (!success)
+            {
+                return new StatusCodeResult(503);
+            }
 
-            return new OkObjectResult(null);
+            return new OkResult();
         }
 
         [FunctionName("GetAllUsers")]
@@ -50,19 +95,60 @@ namespace FriendsAndPlaces.Functions
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "users")] HttpRequest req,
             ILogger log)
         {
-            // Validate Accept header -> HTTP 415
+            // Check if Accept: application/json is present 
+            bool acceptHeaderExists = req.Headers.TryGetValue("Accept", out StringValues acceptHeaders);
+            if (acceptHeaderExists && !acceptHeaders[0].Equals(_acceptHeaderApplicationJson))
+            {
+                return new UnsupportedMediaTypeResult();
+            }
 
-            // Validate query parameters (login, session) -> HTTP 404
+            // Get query parameters
+            string loginName = req.Query["login"];
+            string sessionId = req.Query["session"];
 
-            // Validate session -> HTTP 403
+            // Validate parameters
+            if (string.IsNullOrWhiteSpace(loginName) ||
+                string.IsNullOrWhiteSpace(sessionId))
+            {
+                return new BadRequestResult();
+            }
+
+            // Get session from database
+            string savedSession = _databaseManager.GetSession(loginName);
+
+            // Session for user does not exist
+            if (savedSession == null)
+            {
+                return new UnauthorizedResult();
+            }
+
+            // Sessions do not match
+            if (!sessionId.Equals(savedSession))
+            {
+                return new UnauthorizedResult();
+            }
 
             // Get all users from database
+            var users = _databaseManager.GetAllUsers();
 
-            // Convert users to {loginName, vorname, nachname} objects
+            if (users == null)
+            {
+                return new NotFoundResult();
+            }
 
-            // return users
+            // Convert users to publicUser objects
+            var publicUsers = new List<PublicUser>();
+            foreach (var user in users)
+            {
+                publicUsers.Add(new PublicUser()
+                {
+                    LoginName = user.LoginName,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName
+                });
+            }
 
-            return new OkObjectResult(null);
+            return new OkObjectResult(JsonConvert.SerializeObject(publicUsers));
         }
     }
 }
