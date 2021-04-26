9ec036075dc99d5b898d51912a1e65dd51d9b9b9
Nick Stelzer <mindia620@gmail.com>
2021-04-26 16:17:46 +0200

    overwrite logout to change for dotnet rbeing runnable

--- FriendsAndPlaces/Functions/LogoutFunction.cs
+++ FriendsAndPlaces/Functions/LogoutFunction.cs
@@ -10,7 +10,7 @@ namespace FriendsAndPlaces.Functions
     public class LogoutFunction
     {
         [FunctionName("Logout")]
-        public async Task<IActionResult> Run(
+        public IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
